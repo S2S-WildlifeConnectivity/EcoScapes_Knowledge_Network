23cef6a9b8d750af4d1df7bf2bf6ec2acc8382b4
Niall-NS <nbell@naturesquamish.ca>
2025-10-14 19:36:49 -0700

    Quartz sync: Oct 14, 2025, 7:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "EcoScapes",
-    pageTitleSuffix: "Knowledge Network",
+    pageTitleSuffix: ,
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
