8ecd0d527e429febefafab184245804e1e284faf
Niall-NS <nbell@naturesquamish.ca>
2025-10-14 19:39:07 -0700

    Quartz sync: Oct 14, 2025, 7:39 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      EcoScapes: "https://newdev.naturesquamish.ca/",
-      Nature Squamish: "https://naturesquamish.ca/",
+      GitHub: "https://github.com/jackyzha0/quartz",
+      "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
   }),
 }
